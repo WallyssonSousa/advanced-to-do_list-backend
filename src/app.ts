c98d1c49f19718b054f9ee263a9a2c9dbeaafdd0
WallyssonSousa <wallysson.dev@gmail.com>
2026-03-23 19:45:57 -0300

    fix: adicionando o cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { routes } from "@/infrastructure/http/routes";
+import cors from "cors";
 import { AppDataSource } from "@/infrastructure/persistence/typeorm/data-source";
 
 export const app = express();
 
+app.use(cors()); 
 app.use(express.json());
 app.use(routes);
 
